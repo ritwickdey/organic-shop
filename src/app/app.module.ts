03156e163b4de8da0f8e92f9e85c4bdce934bfcf
Ritwick Dey <ritwickdey@outlook.com>
2017-09-01 19:26:05 +0530

    my-order generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AngularFireModule } from 'angularfire2'
 import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { AngularFireAuthModule } from 'angularfire2/auth'
+
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +11,9 @@ import { ProductsComponent } from './products/products.component';
 import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccssComponent } from './order-succss/order-succss.component';
 import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
     ProductsComponent,
     CheckOutComponent,
     OrderSuccssComponent,
-    ShopingCartComponent
+    ShopingCartComponent,
+    MyOrdersComponent,
+
   ],
   imports: [
     BrowserModule,
