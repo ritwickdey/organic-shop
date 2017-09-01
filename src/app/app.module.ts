f48b46c697cfcc0bfb0ffccea14f8c7d06544582
Ritwick Dey <ritwickdey@outlook.com>
2017-09-01 20:33:52 +0530

    navbar modified & ng-bootstrap package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from 'angularfire2'
 import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { AngularFireAuthModule } from 'angularfire2/auth'
 import { RouterModule } from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
@@ -38,6 +39,7 @@ import { LoginComponent } from './login/login.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'Products', component: ProductsComponent },
