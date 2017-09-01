3a8b3c4218c44616ef1cb518dabb43a24debd66b
Ritwick Dey <ritwickdey@outlook.com>
2017-09-01 19:40:38 +0530

    login component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from 'angularfire2'
 import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { AngularFireAuthModule } from 'angularfire2/auth'
 
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { HomeComponent } from './home/home.component';
@@ -12,10 +13,10 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { OrderSuccssComponent } from './order-succss/order-succss.component';
 import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
-
-import { environment } from './../environments/environment';
 import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +30,7 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     MyOrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
-
+    LoginComponent
   ],
   imports: [
     BrowserModule,
