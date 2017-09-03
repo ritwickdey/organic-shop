21ce20d27b3bab347b991e803a30cc501e813367
Ritwick Dey <ritwickdey@outlook.com>
2017-09-03 14:38:12 +0530

    card added in product form

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,7 +12,9 @@ export class ProductFormComponent implements OnInit {
 
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(categoryService: CategoryService,
+    private productService: ProductService, private router: Router) {
+
     this.categories$ = categoryService.getCatagories();
   }
 
@@ -19,8 +22,9 @@ export class ProductFormComponent implements OnInit {
   }
 
   save(product) {
-    console.log(product);
+    //console.log(product);
     this.productService.create(product);
+    this.router.navigate(['/admin/products']);
   }
 
 }
