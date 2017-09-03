46e59dd0dfad4943460f875766753d4e7d064ff4
Ritwick Dey <ritwickdey@outlook.com>
2017-09-04 00:50:51 +0530

    product card component extracted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { LoginComponent } from './login/login.component';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { FormsModule } from "@angular/forms";
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 
 @NgModule({
@@ -46,7 +47,8 @@ import { ProductFilterComponent } from './products/product-filter/product-filter
     AdminOrdersComponent,
     LoginComponent,
     ProductFormComponent,
-    ProductFilterComponent
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { IProduct } from './../models/product';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent implements OnInit {
+
+  @Input("product") product: IProduct = {} as IProduct;
+  @Input("width") width = "20";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
