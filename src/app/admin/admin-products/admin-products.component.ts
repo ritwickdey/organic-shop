29ea6da96b5ce6e6b56b4ec5d74e6a661e736ee2
Ritwick Dey <ritwickdey@outlook.com>
2017-09-03 16:25:39 +0530

    Searching added

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,36 @@
+import { Subscription } from 'rxjs/Subscription';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  productList$;
+  productList: { title: string }[];
+  filteredProductList: any[];
+  productSubscription: Subscription;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.productList$ = this.productService.getAll();
+    this.productSubscription = this.productService.getAll()
+      .subscribe(products => this.filteredProductList = this.productList = products);
+  }
+
+  ngOnDestroy() {
+    this.productSubscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    if (!query.trim()) {
+      this.filteredProductList = this.productList;
+      return;
+    }
+    this.filteredProductList = this.productList
+      .filter(e => e.title.toLowerCase().indexOf(query.toLowerCase()) !== -1);
   }
 
 }
