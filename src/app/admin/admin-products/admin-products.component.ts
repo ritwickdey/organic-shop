bbd063cfad1add360fea00dc2985c61751a82418
Ritwick Dey <ritwickdey@outlook.com>
2017-09-03 14:54:08 +0530

    product of list implemented

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+  productList$;
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productList$ = this.productService.getAll();
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,4 +11,8 @@ export class ProductService {
     return this.db.list('/products').push(product);
   }
 
+  getAll() {
+    return this.db.list('/products');
+  }
+
 }
