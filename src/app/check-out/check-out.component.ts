af635351d2f1a65eb8449f90bf2d55a96f066b51
Ritwick Dey <ritwickdey@outlook.com>
2017-09-05 18:40:31 +0530

    checkout component refactored

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,26 +1,23 @@
+import { Observable } from 'rxjs/Observable';
 import { ShoppingCart } from './../models/shopping-cart';
 import { Subscription } from 'rxjs/Subscription';
 import { ShoppingCartService } from './../shopping-cart.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy {
-  carts: ShoppingCart;
-  cartSubscription: Subscription;
+export class CheckOutComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
 
   constructor(private cartService: ShoppingCartService) { }
 
   async ngOnInit() {
-    this.cartSubscription = (await this.cartService.getCart())
-      .subscribe(carts => this.carts = carts);
+    this.cart$ = await this.cartService.getCart();
   }
 
-  ngOnDestroy() {
-    this.cartSubscription.unsubscribe();
-  }
+
 
 }
