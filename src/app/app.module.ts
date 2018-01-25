f15d9b14c4c7ed95d56eb1e768e9501848a7d6fe
Ritwick Dey <ritwickdey@outlook.com>
2018-01-26 00:39:44 +0530

    ShoppingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,15 +22,8 @@ import { OrderService } from './shared/services/order.service';
 import { ProductService } from './shared/services/product.service';
 import { ShoppingCartService } from './shared/services/shopping-cart.service';
 import { UserService } from './shared/services/user.service';
-import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
-import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
-import { OrderSuccssComponent } from './shopping/components/order-succss/order-succss.component';
-import { OrderViewComponent } from './shopping/components/order-view/order-view.component';
-import { ProductFilterComponent } from './shopping/components/products/product-filter/product-filter.component';
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShippingFormComponent } from './shopping/components/shipping-form/shipping-form.component';
-import { ShopingCartComponent } from './shopping/components/shoping-cart/shoping-cart.component';
-import { ShoppingCartSummaryComponent } from './shopping/components/shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingModule } from './shopping/shopping.module';
 
 
 
@@ -38,16 +31,7 @@ import { ShoppingCartSummaryComponent } from './shopping/components/shopping-car
   declarations: [
     AppComponent,
     BsNavbarComponent,
-    ProductsComponent,
-    CheckOutComponent,
-    OrderSuccssComponent,
-    ShopingCartComponent,
-    MyOrdersComponent,
-    LoginComponent,
-    ProductFilterComponent,
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent,
-    OrderViewComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -60,16 +44,10 @@ import { ShoppingCartSummaryComponent } from './shopping/components/shopping-car
     DataTableModule,
     SharedModule,
     AdminModule,
+    ShoppingModule,
     RouterModule.forRoot([
       { path: '', component: ProductsComponent },
-      { path: 'Products', component: ProductsComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'cart', component: ShopingCartComponent },
-
-      { path: 'myorders', component: MyOrdersComponent, canActivate: [AuthGuardService] },
-      { path: 'checkout', component: CheckOutComponent, canActivate: [AuthGuardService] },
-      { path: 'order-success/:id', component: OrderSuccssComponent, canActivate: [AuthGuardService] },
-      { path: 'order-details/:id', component: OrderViewComponent, canActivate: [AuthGuardService] }
+      { path: 'login', component: LoginComponent }
     ])
   ],
   providers: [
---
+++ src/app/shopping/shopping.module.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthGuardService } from 'shared/services/auth-guard.service';
+import { SharedModule } from 'shared/shared.module';
+
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { OrderSuccssComponent } from './components/order-succss/order-succss.component';
+import { OrderViewComponent } from './components/order-view/order-view.component';
+import { ProductFilterComponent } from './components/products/product-filter/product-filter.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShippingFormComponent } from './components/shipping-form/shipping-form.component';
+import { ShopingCartComponent } from './components/shoping-cart/shoping-cart.component';
+import { ShoppingCartSummaryComponent } from './components/shopping-cart-summary/shopping-cart-summary.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule,
+    RouterModule.forChild([
+    { path: 'cart', component: ShopingCartComponent },
+    { path: 'Products', component: ProductsComponent },
+    { path: 'myorders', component: MyOrdersComponent, canActivate: [AuthGuardService] },
+    { path: 'checkout', component: CheckOutComponent, canActivate: [AuthGuardService] },
+    { path: 'order-success/:id', component: OrderSuccssComponent, canActivate: [AuthGuardService] },
+    { path: 'order-details/:id', component: OrderViewComponent, canActivate: [AuthGuardService] }
+  ])
+  ],
+  declarations: [
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccssComponent,
+    ShopingCartComponent,
+    MyOrdersComponent,
+    ProductFilterComponent,
+    ShoppingCartSummaryComponent,
+    ShippingFormComponent,
+    OrderViewComponent
+  ]
+})
+export class ShoppingModule { }
